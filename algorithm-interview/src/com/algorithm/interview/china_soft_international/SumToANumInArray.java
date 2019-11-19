4a9af1c7ffd412ac164cd32714a5b9cdd46ef380
李非凡 <897199688@qq.com>
2019-11-19 21:49:38 +0800

    极客时间

---
+++ algorithm-interview/src/com/algorithm/interview/china_soft_international/SumToANumInArray.java
@@ -0,0 +1,19 @@
+package com.algorithm.interview.china_soft_international;
+
+/**
+ * @Author 李非凡
+ * @Description:
+ * 从一个有序数组里找到两个数字的和等于一个目标数字，打印出来这两个数字，要求算法和复杂度为O(n)，怎么解决？
+ * @Date 2019/11/18 22:26
+ * @Version 1.0
+ */
+public class SumToANumInArray {
+
+    private static void getSumToANum(int[] array, int num) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
