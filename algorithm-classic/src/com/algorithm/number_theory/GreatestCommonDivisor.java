67c265778c1e0fd3d2283eb721e7c353c3a8f4a8
liff14 <liff14@lenovo.com>
2019-11-20 10:08:58 +0800

    Java常用算法手册

---
+++ algorithm-classic/src/com/algorithm/number_theory/GreatestCommonDivisor.java
@@ -0,0 +1,25 @@
+package com.algorithm.number_theory;
+
+/**
+ * @Author 李非凡
+ * @Description:
+ * 最大公约数（GCD）
+ * @Date 2019/11/19 18:19
+ * @Version 1.0
+ */
+public class GreatestCommonDivisor {
+
+    /**
+     * 辗转相除法，即欧几里得算法（Euclidean algorithm）
+     * @param a
+     * @param b
+     * @return
+     */
+    private static int divisionAlgorithm(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
